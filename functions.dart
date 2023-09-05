a8da0c24e34fdf89019cbcd03c313fb089fcf0c6
syedhariz1995 <syedhariz1995@gmail.com>
2023-09-05 16:28:58 +0800

    added functions notes and file

---
+++ functions.dart
@@ -0,0 +1,45 @@
+  // // Functions in Dart
+  // // Syntax : <dataype> fnName() { --code here}   
+  // // In Dart, the functions are usually outside of main
+  // // In Dart, you need to call function within void main().
+  // // In Dart, void main() is a MUST HAVE. That's the entry point of Dart to run their code
+  // // In Dart, functions are not constant value. Don't use const
+
+void main(){
+  // // Call the functions
+  printName();         // can call void function without print
+  print(printGame());  // if its not void, call it with print
+  print(printNum());   // if its not void, call it with print
+
+  int num = printNum(); // Assign function to variable, call it with print. But make sure same datatype as function (int to int)
+  print(num);          
+
+  print(getMultipleValues());  // (34, Laptop, true, 3.24)
+  print(getMultipleValues().$2); // Laptop   // Can access properties. getters ike $1, $2 etc shows the order if you want the specifics.
+
+  // // Destructure works too
+  var (age, item, isAdult, price) = getMultipleValues();
+  print(age); // Gets the int, passed fro, getMultipleValue()
+  print(item); // Gets the String, passed fro, getMultipleValue()
+  print(isAdult); // Gets the bool, passed fro, getMultipleValue()
+  print(price); // Gets the double, passed fro, getMultipleValue()
+}
+
+// // Outside of main()
+void printName(){
+  print("Hariz");
+}
+
+String printGame(){
+  return "Monopoly";
+}
+
+int printNum(){
+  return 120;
+}
+
+
+// // functions with multiple datatype. Use "records" syntax : (datatype1, datatype2) fnName(){return (value of datatype1, value of datatype2)}
+(int,String, bool, double) getMultipleValues(){
+  return (34, 'Laptop' , true, 3.24);
+} 
